d0612cbb33a6f0c4bbeae2595b8c8f810bda00c8
Berk Karabacak <berkkarabacak@gmail.com>
2023-08-06 16:34:26 +0300

    added alibi

--- model.py
+++ model.py
@@ -26,6 +26,71 @@ class LayerNorm(nn.Module):
     def forward(self, input):
         return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
 
+def get_slopes(n_heads: int):
+    """
+    ## Get head-specific slope $m$ for each head
+
+    * `n_heads` is the number of heads in the attention layer $n$
+
+    The slope for first head is
+
+    $$\frac{1}{2^{\frac{8}{n}}} = 2^{-\frac{8}{n}}$$
+
+    The slopes for the rest of the heads are in a geometric series with a ratio same as above.
+
+    For instance when the number of heads is $8$ the slopes are
+    $$\frac{1}{2^1}, \frac{1}{2^2}, \dots, \frac{1}{2^8}$$
+    """
+
+    # Get the closest power of 2 to `n_heads`.
+    # If `n_heads` is not a power of 2, then we first calculate slopes to the closest (smaller) power of 2,
+    # and then add the remaining slopes.
+    n = 2 ** math.floor(math.log2(n_heads))
+    # $2^{-\frac{8}{n}}$
+    m_0 = 2.0 ** (-8.0 / n)
+    # $2^{-1\frac{8}{n}}, 2^{-2 \frac{8}{n}}, 2^{-3 \frac{8}{n}}, \dots$
+    m = torch.pow(m_0, torch.arange(1, 1 + n))
+
+    # If `n_heads` is not a power of 2, then we add the remaining slopes.
+    # We calculate the remaining slopes for $n * 2$ (avoiding slopes added previously).
+    # And pick the slopes upto `n_heads`.
+    if n < n_heads:
+        # $2^{-\frac{8}{2n}}$
+        m_hat_0 = 2.0 ** (-4.0 / n)
+        # $2^{-1\frac{8}{2n}}, 2^{-3 \frac{8}{2n}}, 2^{-5 \frac{8}{2n}}, \dots$
+        # Note that we take steps by $2$ to avoid slopes added previously.
+        m_hat = torch.pow(m_hat_0, torch.arange(1, 1 + 2 * (n_heads - n), 2))
+        # Concatenate the slopes with the remaining slopes.
+        m = torch.cat([m, m_hat])
+
+    return m
+
+@torch.no_grad()
+def get_alibi_biases(n_heads: int, mask: torch.Tensor):
+    """
+    ## Calculate the attention biases matrix
+
+    * `n_heads` is the number of heads in the attention layer
+    * `mask` is the attention mask of shape `[seq_len_q, seq_len_k]`
+
+    This returns a matrix of shape `[seq_len_q, seq_len_k, n_heads, ]` with ALiBi attention biases.
+    """
+
+    # Get slopes $m$ for each head
+    m = get_slopes(n_heads).to(mask.device)
+
+    # Calculate distances $[0, 1, \dots, N]$
+    # Here we calculate the distances using the mask.
+    #
+    # Since it's causal mask we can just use $[0, 1, \dots, N]$ too.
+    # `distance = torch.arange(mask.shape[1], dtype=torch.long, device=mask.device)[None, :]`
+    distance = mask.cumsum(dim=-1)
+
+    # Multiply them pair-wise to get the AliBi bias matrix
+    return distance[:, :, None] * m[None, None, :]
+
+
+
 class CausalSelfAttention(nn.Module):
 
     def __init__(self, config):
@@ -48,6 +113,10 @@ class CausalSelfAttention(nn.Module):
             # causal mask to ensure that attention is only applied to the left in the input sequence
             self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
                                         .view(1, 1, config.block_size, config.block_size))
+        # Get slopes for each head
+        self.register_buffer('slopes', get_slopes(config.n_head))
+
+        self.alibi_biases = None
 
     def forward(self, x):
         B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
@@ -63,12 +132,26 @@ class CausalSelfAttention(nn.Module):
             # efficient attention using Flash Attention CUDA kernels
             y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
         else:
-            # manual implementation of attention
+            # Compute attention scores
             att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-            att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+
+            # Create AliBi biases if it's not cached
+            if self.alibi_biases is None or self.alibi_biases.shape[1] < T:
+                # `mask` is self.bias[:,:,:T,:T]
+                self.alibi_biases = get_alibi_biases(att.size(-1), self.bias[:, :, :T, :T][:, :, 0, 0])
+
+            # Add AliBi biases to attention scores.
+            att += self.alibi_biases[:T, :T, None, :]
+
+            # Apply mask
+            att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
+
+            # Apply softmax and dropout
             att = F.softmax(att, dim=-1)
             att = self.attn_dropout(att)
-            y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+
+            # Multiply by values
+            y = att @ v
         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
 
         # output projection
