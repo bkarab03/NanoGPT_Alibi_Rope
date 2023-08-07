cd94f571da392e3ef7d5dfe0861f70a0aeefc5a8
Berk Karabacak <berkkarabacak@gmail.com>
2023-08-08 02:34:13 +0300

    Working on ROPE

--- model.py
+++ model.py
@@ -203,60 +203,34 @@ class GPTConfig:
 
 
 class RotaryPositionalEmbeddings(nn.Module):
-    """
-    Rotary Positional Embeddings (RoPE) implementation.
-    """
-
     def __init__(self, d: int, base: int = 10_000):
-        """
-        Initialize the RoPE.
-
-        Args:
-            d (int): Number of features.
-            base (int, optional): Constant used for calculating Θ. Defaults to 10_000.
-        """
         super().__init__()
-
         self.base = base
         self.d = d
         self.cos_cached = None
         self.sin_cached = None
 
     def _build_cache(self, x: torch.Tensor):
-        if self.cos_cached is not None and x.shape[1] <= self.cos_cached.shape[1]:
+        if self.cos_cached is not None and x.shape[0] <= self.cos_cached.shape[0]:
             return
-
-        seq_len = x.shape[1]
+        seq_len = x.shape[0]
         theta = 1. / (self.base ** (torch.arange(0, self.d, 2).float() / self.d)).to(x.device)
         seq_idx = torch.arange(seq_len, device=x.device).float().to(x.device)
         idx_theta = torch.einsum('n,d->nd', seq_idx, theta)
         idx_theta2 = torch.cat([idx_theta, idx_theta], dim=1)
-        # print("Shape of idx_theta:", idx_theta.shape)
-        # print("Shape of idx_theta2:", idx_theta2.shape)
-
-        self.cos_cached = idx_theta2.cos()[None, :, :]
-        self.sin_cached = idx_theta2.sin()[None, :, :]
-
-    def forward(self, x: torch.Tensor):
-        self._build_cache(x)
-
-        x_rope, x_pass = x[..., :self.d], x[..., self.d:]
-        neg_half_x = self._neg_half(x_rope)
-        x_rope = (x_rope * self.cos_cached[:, :x.shape[1], :]) + (neg_half_x * self.sin_cached[:, :x.shape[1], :])
-        # print("Shape of x_rope:", x_rope.shape)
-        # print("Shape of neg_half_x:", neg_half_x.shape)
-
-
-        return torch.cat((x_rope, x_pass), dim=-1)
-
+        self.cos_cached = idx_theta2.cos()[:, None, None, :]
+        self.sin_cached = idx_theta2.sin()[:, None, None, :]
 
     def _neg_half(self, x: torch.Tensor):
         d_2 = self.d // 2
-        x[:, :, d_2:] = -x[:, :, d_2:]
-        return x
+        return torch.cat([-x[:, d_2:], x[:, :d_2]], dim=-1)
 
-
-
+    def forward(self, x: torch.Tensor):
+        self._build_cache(x)
+        x_rope, x_pass = x[..., :self.d], x[..., self.d:]
+        neg_half_x = self._neg_half(x_rope)
+        x_rope = (x_rope * self.cos_cached[:x.shape[0]]) + (neg_half_x * self.sin_cached[:x.shape[0]])
+        return torch.cat((x_rope, x_pass), dim=-1)
 
 class GPT(nn.Module):
 
